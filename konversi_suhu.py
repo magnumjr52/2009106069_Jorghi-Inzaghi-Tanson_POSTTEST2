53fc5610a7acaf2428087e057f10734da76d93d7
magnumjr52 <74251422+magnumjr52@users.noreply.github.com>
2020-11-10 22:11:00 +0800

    Add files via upload

---
+++ konversi_suhu.py
@@ -0,0 +1,26 @@
+print("")
+print("Post-Test Ke-2")
+print("Tema : Konversi Suhu")
+print("By Jorghi Inzaghi Tanson")
+print("")
+
+print("Konversi Suhu celcius Ke Reamur \n°Ré = °C × 0,8")
+C = int(input("Masukkan Nilai Celcius : "))
+R = C * 0.8
+print("Hasil Konversi :", R, "°Ré")
+print("")
+
+print("Konversi Suhu Celcius Ke Fahrenheit \n°F = °C × 1,8 + 32")
+C = int(input("Masukkan Nilai Celcius : "))
+F = (C * 1.8) + 32
+print("Hasil Konversi :", F, "°F")
+print("")
+
+print("Konversi Suhu Celcius ke Kelvin \nK = °C + 273,15")
+C = int(input("Masukkan Nilai Celcius : "))
+K = C + 273.15
+print("Hasil Konversi :", K, "K")
+print("")
+
+print("Program Selesai")
+print("")
---
+++ laptop.py
@@ -0,0 +1,46 @@
+print("")
+print("Post-Test Ke-2")
+print("Tema : Laptop")
+print("By Jorghi Inzaghi Tanson")
+print("")
+
+print("Daftar Laptop")
+laptop1 = input("1) ")
+laptop2 = input("2) ")
+laptop3 = input("3) ")
+print("")
+print("Harga Tiap Barang (Dalam Juta Rupiah)")
+harga1 = float(input("1) "))
+harga2 = float(input("2) "))
+harga3 = float(input("3) "))
+print("")
+print("Stok Tersedia")
+stok1 = int(input("1) "))
+stok2 = int(input("2) "))
+stok3 = int(input("3) "))
+print("")
+
+laptop = [laptop1, laptop2, laptop3]
+harga = [harga1, harga2, harga3]
+stok = [stok1, stok2, stok3]
+
+print("Deskripsi Barang Pertama")
+print("Nama :", laptop[0])
+print("Harga :", harga[0], "Juta Rupiah")
+print("Stok :", stok[0])
+print("")
+
+print("Deskripsi Barang Kedua")
+print("Nama :", laptop[1])
+print("Harga :", harga[1], "Juta Rupiah")
+print("Stok :", stok[1])
+print("")
+
+print("Deskripsi Barang Ketiga")
+print("Nama :", laptop[2])
+print("Harga :", harga[2], "Juta Rupiah")
+print("Stok :", stok[2])
+print("")
+
+print("Program Selesai")
+print("")
